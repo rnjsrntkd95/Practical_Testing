11926e2bfbd445be90e2d0eec6488aa0d03629f6
kks1023 <kks1023@midasin.com>
2023-08-10 17:42:32 +0900

    lecture: JUnit5로 테스트하기
    섹션 2.단위 테스트

--- src/test/java/com/example/cafe_kiosk/unit/CafeKioskTest.java
+++ src/test/java/com/example/cafe_kiosk/unit/CafeKioskTest.java
@@ -1,16 +1,52 @@
 package com.example.cafe_kiosk.unit;
 
 import com.example.cafe_kiosk.unit.beverage.Americano;
+import com.example.cafe_kiosk.unit.beverage.Latte;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CafeKioskTest {
+
+    @Test
+    void add_menual_test() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+    }
 
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
-        System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
-        System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages()).hasSize(0);
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+
+        cafeKiosk.add(new Americano());
+        cafeKiosk.add(new Latte());
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages()).hasSize(0);
     }
 }
